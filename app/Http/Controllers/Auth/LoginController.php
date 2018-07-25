2cd80cebc6e681eae91140909476e0b031754f6b
WebDever <webdevermail@yahoo.com>
2018-07-25 14:00:22 +0200

    Login und registration redirection changed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/calculator';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
