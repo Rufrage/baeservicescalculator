f4b843e1b61b4d0f4a4a94a8b5780c883f2f06a9
WebDever <webdevermail@yahoo.com>
2018-10-30 16:25:27 +0100

    debug

--- resources/views/umzug/allgemeines.blade.php
+++ resources/views/umzug/allgemeines.blade.php
@@ -134,7 +134,7 @@
         <div class="col-sm-6">
             {!! Form::label('übersee_lokal_2', 'Übersee / Lokal') !!}
             {{ $umzug->übersee_lokal_2 }}
-            {!! Form::select('übersee_lokal_2', ['Ü' => 'Übersee', 'L' => 'Lokal'], null, ['class'=>'form-control', 'id'=>'übersee_lokal_2'])  !!}
+            {!! Form::select('übersee_lokal_2', ['Ü' => 'Übersee', 'L' => 'Lokal'], $umzug->übersee_lokal_2, ['class'=>'form-control', 'id'=>'übersee_lokal_2'])  !!}
         </div>
 
         <div class="col-sm-6">
