7488864584c6386d9766c82b243fced11bad5855
Janosch <janoschvankann@hotmail.com>
2018-11-28 20:12:21 +0100

    Bilderanfangsbuchstaben groß

--- resources/views/umzug/kinderzimmer.blade.php
+++ resources/views/umzug/kinderzimmer.blade.php
@@ -84,7 +84,7 @@
             {!! Form::number('kinderzimmer_kommode_klein', null, ['class'=>'form-control']) !!}
         </div>
         <div class="col-sm-6 col-lg-4">
-            <img src="{{ asset('images/moebel/schrank_klein.svg') }}" alt="" height="64px">
+            <img src="{{ asset('images/moebel/Schrank_klein.svg') }}" alt="" height="64px">
             {!! Form::label('kinderzimmer_schrank_klein', 'Schrank (klein)') !!}
             {!! Form::number('kinderzimmer_schrank_klein', null, ['class'=>'form-control']) !!}
         </div>
@@ -99,13 +99,13 @@
             {!! Form::number('kinderzimmer_schrank3', null, ['class'=>'form-control']) !!}
         </div>
         <div class="col-sm-6 col-lg-4">
-            <img src="{{ asset('images/moebel/schrank_hochschrank.svg') }}" alt="" height="64px">
+            <img src="{{ asset('images/moebel/Schrank_hochschrank.svg') }}" alt="" height="64px">
             {!! Form::label('kinderzimmer_hochschrank', 'Schrank, Hochschrank') !!}
             {!! Form::number('kinderzimmer_hochschrank', null, ['class'=>'form-control']) !!}
         </div>
 
         <div class="col-sm-6 col-lg-4">
-            <img src="{{ asset('images/moebel/sideboard_mittel.svg') }}" alt="" height="64px">
+            <img src="{{ asset('images/moebel/Sideboard_mittel.svg') }}" alt="" height="64px">
             {!! Form::label('kinderzimmer_sideboard_mittel', 'Sideboard (mittel)') !!}
             {!! Form::number('kinderzimmer_sideboard_mittel', null, ['class'=>'form-control']) !!}
         </div>
